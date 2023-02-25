e730de3ea26ba23871f9309e1e434ae4e225f64e
MatildeF25 <73347804+MatildeF25@users.noreply.github.com>
2023-02-25 00:11:28 +0000

    Update 100q.c

--- 100q/100q.c
+++ 100q/100q.c
@@ -3,6 +3,17 @@ typedef struct lligada {
     struct lligada *prox;
 } *LInt;
 
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+
 int length (LInt l){
     int i;
     while (l)
@@ -28,4 +39,63 @@ void imprimeL (LInt l){
         l = l->prox;
     }
 
+}
+
+LInt reverseL (LInt l){
+    if(!l){
+        return NULL;
+    }
+    LInt ant=NULL, next;
+
+    while(l){
+        next = l->prox;
+        l->prox = ant;
+        ant = l;
+        l = next;
+    }
+
+    return ant;
+}
+
+void insertOrd (LInt *l, int x){
+    LInt new = newLInt(x,NULL);
+    LInt ant = NULL;
+   
+    while ((*l) && (*l)->valor <= x){
+        ant = *l;
+        l = &(*l)->prox;
+    }
+
+    if(ant){
+        new->prox = *l;
+        ant->prox = new;
+    }
+    else{
+        new->prox = (*l);
+        *l = new;
+    }
+    
+}
+
+int removeOneOrd (LInt *l, int x){
+    LInt ant = NULL;
+
+    while((*l) && (*l)->valor != x){
+        ant = *l;
+        l = &(*l)->prox;
+    }
+
+    if(l==NULL){
+        return 1;
+    }
+
+    if(ant){
+        ant->prox = &(*l)->prox;
+        free(l);
+        return 0;
+    }
+    else{
+        *l = &(*l)->prox;
+        return 0;
+    }
 }
