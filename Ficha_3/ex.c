e86c5ced26685ae25b2c9b31bc250da7d6376667
MatildeFe25 <matilde2002fe@gmail.com>
2023-03-10 11:37:42 +0000

    Ficha 3

---
+++ Ficha_3/ex.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void swapM (int *x, int *y){
+    int aux = *y;
+    *y = *x;
+    *x = aux;
+}
+
+void swap (int v[], int i, int j){
+    int aux = v[i];
+    v[i]=v[j];
+    v[j]=aux;
+}
+
+int soma (int v[], int N){
+    int res;
+    for(int i = 0; i<N;i++){
+        res += v[i];
+    }
+    return res;
+}
+
+void inverteArray1 (int v[], int N){
+    for(int i = 0; i<N/2; i++){
+        swap(v, i, N-i-1);
+    }
+}
+
+void inverteArray2 (int v[], int N){
+    for(int i = 0; i<N/2; i++){
+        swapM(&v[i],&v[N-1-i]);
+    }
+}
+
+int maximum (int v[], int N, int *m){
+    if(N>0){
+        for (int i = 0; i < N; i++){
+            if(v[i]>*m){
+            *m = v[i];
+            }
+    
+        }
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+
+
+int main() {
+        int arr[] = {1, 4, 2, 7, 5};
+    int N = 5;
+    int max;
+    if (maximum(arr, N, &max) == 0) {
+        printf("The maximum element is %d\n", max);
+    }
+    else {
+        printf("Error: the array is empty\n");
+    }
+    return 0;
+}
