a018bd219a868b2d9c4a40c6c3fdca29e2ff670b
MatildeFe25 <matilde2002fe@gmail.com>
2023-05-16 23:56:20 +0100

    cenas

--- 100q/100q.c
+++ 100q/100q.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 
 typedef struct lligada {
@@ -595,6 +596,7 @@ void mirror (ABin *a){
     if((*a)==NULL){
         return;
     }
+
     ABin aux = (*a)->dir;
     (*a)->dir = (*a)->esq;
     (*a)->esq = aux;
@@ -602,48 +604,379 @@ void mirror (ABin *a){
     mirror(&(*a)->esq);
 }
 
-void inorder(ABin a, LInt* l) {
+void inorder(ABin a, LInt *l) {
     if (a == NULL) {
         return;
     }
     
-    inorder(a->esq, l);  // Traverse the left subtree recursively
+    inorder(a->esq, l);
+
+    LInt new_node = newLInt(a->valor, NULL);
     
-    // Create a new node for the current element and append it to the linked list
-    LInt new_node = newLInt2(a->valor, NULL);
-    if (*l == NULL) {
-        *l = new_node;  // If the linked list is empty, set the new node as the head
-    } else {
-        LInt current = *l;
-        while (current->prox != NULL) {
-            current = current->prox;  // Traverse to the last node
+    if(*l == NULL){
+        *l = new_node;
+    }
+    else{
+        LInt atual = *l;
+        while (atual->prox != NULL){
+            atual = atual->prox;
         }
-        current->prox = new_node;  // Append the new node to the last node
+        atual->prox = new_node;
+        
+    }  
+    
+    inorder(a->dir, l);
+}
+
+void preorder (ABin a, LInt *l){
+    if(a == NULL){
+        return;
     }
     
-    inorder(a->dir, l);  // Traverse the right subtree recursively
+    LInt new_node = newLInt(a->valor, NULL);
+    if(*l == NULL){
+        *l = new_node;
+    }
+    else{
+        LInt atual = *l;
+        while (atual->prox != NULL){
+            atual = atual->prox;
+        }
+        atual->prox = new_node;
+    }
+
+    preorder(a->esq, l);
+    preorder(a->dir, l);
+}
+
+void posorder (ABin a, LInt *l){
+    if(a == NULL){
+        return;
+    }
+    
+    posorder(a->esq, l);
+    posorder(a->dir, l);
+    
+    LInt new_node = newLInt(a->valor, NULL);
+    if(*l == NULL){
+        *l = new_node;
+    }
+    else{
+        LInt atual = *l;
+        while (atual->prox != NULL){
+            atual = atual->prox;
+        }
+        atual->prox = new_node;
+    }
+
 }
 
 
+int depth (ABin a, int x){
+    if(a == NULL){
+        return -1;
+    }
+    if(a->valor == x){
+        return 1;
+    }
+    int ld = depth(a->esq,x);
+    int rd = depth(a->dir,x);
+    if(ld == -1 && rd == -1) return -1;
+    if(ld == -1) return rd + 1;
+    if(rd == -1) return rd + 1;
 
+    if(ld<rd){
+        return ld + 1;
+    }
+    else{
+        return rd + 1;
+    }
+}
+
+int freeAB (ABin a){
+    if(a == NULL){
+        return 0;
+    }
+
+    int c = freeAB(a->esq);
+    int c2 = freeAB(a->dir);
+    free(a);
+    return(c+c2+1);
+}
+
+int pruneAB(ABin *a, int l) {
+    if (*a == NULL) {
+        return 0;
+    }
+
+    int c = pruneAB(&(*a)->esq, l-1);
+    int c2 = pruneAB(&(*a)->dir, l-1);
+
+    if (l<=0) {
+        free(*a);
+        *a = NULL;
+        return c + c2 + 1;
+    }
+
+    return c + c2;
+}
+
+int iguaisAB (ABin a, ABin b){
+    if(a==NULL && b==NULL){
+        return 1;
+    }    
+    if(a==NULL){
+        return 0;
+    }
+    if(b==NULL){
+        return 0;
+    }
+
+    if(a->valor != b->valor){
+        return 0;
+    }
+
+    int l = iguaisAB(a->esq,b->esq);
+    int r = iguaisAB(a->dir,b->dir);
+    if(l==1 && r==1){
+        return 1;
+    }
+    return 0;    
+}
+
+LInt nivelL(ABin a, int n) {
+    if (a == NULL || n < 1) {
+        return NULL;
+    }
+
+    if (n == 1) {
+        return newLInt(a->valor, NULL);
+    }
+
+    LInt left = nivelL(a->esq, n - 1);
+    LInt right = nivelL(a->dir, n - 1);
+
+    if(l2 == NULL){
+        return l1;
+    }
+    LInt l = l1;
+    while (l->prox!=NULL){
+        l = l->prox;
+    }
+    l->prox = l2;
+    return l1;
+}
+
+int nivelV (ABin a, int n, int v[]){
+    if(a==NULL){
+        return 0;
+    }
+
+    if(n==1){
+        v[0] = a->valor;
+        return 1;
+    }
+
+    int l = nivelL(a->esq, n-1, v);
+    int r = nivelL(a->dir, n-1, v+l);
+
+    return l+r;
+}
+
+int dumpAbin(ABin a, int v[], int N) {
+    if (a == NULL || N <= 0) {
+        return 0;
+    }
+
+    int l = dumpAbin(a->esq, v, N);
+
+    if (l >= N) {
+        return l;
+    }
+
+    v[l] = a->valor;
+
+    int r = dumpAbin(a->dir, v, N - l - 1);
+
+    return l + r + 1;
+}
+
+ABin somasAcA (ABin a){
+    if(a==NULL){
+        return NULL;
+    }
+
+    ABin new = malloc(sizeof(struct nodo));
+    new->valor = a->valor;
+    new->esq = somasAcA(a->esq);
+    new->dir = somasAcA(a->dir);
+
+    if(new->esq != NULL){
+        new->valor += new->esq->valor;
+    }
+    if(new->esq != NULL){
+        new->valor += new->dir->valor;
+    }
+    return new;
+}
+
+int contaFolhas (ABin a){
+    if(a==NULL){
+        return 0;
+    }
+
+    int l = contaFolhas(a->esq);
+    int r = contaFolhas(a->dir);
+
+    if(l==0 && r==0){
+        return 1
+    }
+
+    return l+r;
+}
+
+ABin cloneMirror (ABin a){
+    if(a==NULL){
+        return NULL;
+    }
+
+    ABin new = malloc(sizeof(structnodo));
+    new->valor = a->valor;
+    new->esq = cloneMirror(a->dir);
+    new->dir = cloneMirror(a->esq);
+
+    return new;
+}
+
+int addOrd (ABin *a, int x){
+    while (*a != NULL){
+        if ((*a)->valor == x){
+            return 1;
+        }
+        if ((*a)->valor > x){
+            a = &((*a)->esq);
+        }
+        else{
+            a = &((*a)->dir);
+        }
+    }
+
+    *a = malloc(sizeof(struct nodo));
+    (*a)->valor = x;
+    (*a)->esq = NULL;
+    (*a)->dir = NULL;
+
+    return 0;    
+}
+
+int lookupAB (ABin a, int x){
+    while (a != NULL){
+        if(a->valor == x){
+            return 1;
+        }
+        if(a->valor > x){
+           a = a->esq;  
+        } 
+        else{
+            a = a->dir;
+        }
+    }
+    
+    return 0;
+}
+
+int depthOrd (ABin a, int x){
+    int r = 1;
+    while (a != NULL){
+        if(a->valor == x){
+            return r;
+        }
+        if(a->valor > x){
+            a = a->esq;
+            r++;
+        }
+        else{
+            a = a->dir;
+            r++;
+        }
+    }
+
+    return -1;
+}
+
+int maiorAB (ABin a){
+    int maior = 0;
+    while (a!=NULL){
+        maior = a->valor;
+        a = a->dir;
+    }
+
+    return maior;
+}
+
+void removeMaiorA (ABin *a){
+    while ((*a)->dir != NULL){
+        a = &(*a)->dir;        
+    }
+
+    ABin temp = *a;
+    *a = (*a)->esq;
+    free(temp);
+}
+
+int quantosMaiores (ABin a, int x){
+    if(a == NULL){
+        return
+    }
+}
+
+
+ABin createBinaryTree() {
+    ABin root = (ABin) malloc(sizeof(struct nodo));
+    root->valor = 1;
+
+    root->esq = (ABin) malloc(sizeof(struct nodo));
+    root->esq->valor = 2;
+    root->esq->esq = (ABin) malloc(sizeof(struct nodo));
+    root->esq->esq->valor = 4;
+    root->esq->esq->esq = NULL;
+    root->esq->esq->dir = NULL;
+    root->esq->dir = (ABin) malloc(sizeof(struct nodo));
+    root->esq->dir->valor = 5;
+    root->esq->dir->esq = NULL;
+    root->esq->dir->dir = NULL;
+
+    root->dir = (ABin) malloc(sizeof(struct nodo));
+    root->dir->valor = 3;
+    root->dir->esq = (ABin) malloc(sizeof(struct nodo));
+    root->dir->esq->valor = 6;
+    root->dir->esq->esq = NULL;
+    root->dir->esq->dir = NULL;
+    root->dir->dir = (ABin) malloc(sizeof(struct nodo));
+    root->dir->dir->valor = 7;
+    root->dir->dir->esq = NULL;
+    root->dir->dir->dir = NULL;
+
+    return root;
+}
 
 
 int main (){
-    // create a linked list 1->2->3->NULL
-    LInt l = newLInt(1, newLInt(2, newLInt(3, NULL)));
-    
-    LInt clone = cloneRev(l);
+    ABin a = createBinaryTree();
+    LInt l = NULL;
 
-    // print the original list
-    printf("Original list: ");
-    imprimeL(l);
+    posorder(a, &l);
 
-    // print the cloned list in reverse order
-    printf("Cloned list (in reverse order): ");
-    imprimeL(clone);
+    while (l!=NULL){
+        printf("%d",l->valor);
+        l = l->prox;
+    }
+    printf("\n");
 
+    int r = depth(a,1);
 
-    freeL(l);
+    printf("%d\n",r);
+
     return 0;
 }
 
